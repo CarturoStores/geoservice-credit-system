efb20931c6f8c5078d3b3ef5acdd948574e91697
David Perez <kitsunebrave@gmail.com>
2018-12-13 18:55:41 -0500

    geoservice footer

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer
+        className="footer bg-dark text-white mt-5 p-4 text-center"
+        style={{
+          backgroundColor: "#F8F8F8",
+          borderTop: "0px solid #E7E7E7",
+          textAlign: "center",
+          padding: "0px",
+          position: "fixed",
+          left: "0",
+          bottom: "0px",
+          height: "60px",
+          width: "100%"
+        }}
+      >
+        Copyright &copy; {new Date().getFullYear()} Google Georservice
+      </footer>
+    );
+  }
+}
+
+export default Footer;
