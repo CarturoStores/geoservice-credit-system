94b9662dee487690c9ad2e9ddd220545fb0e7184
David Perez <kitsunebrave@gmail.com>
2018-12-12 23:50:59 -0500

    login & current  route

--- routes/api/users.js
+++ routes/api/users.js
@@ -73,6 +73,71 @@ router.post('/register', (req, res) => {
         });
       }
     })
-})
+});
+
+// @route   GET api/users/login
+// @desc    Login user
+// @access  Public
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ where: { email }})
+    .then(user => {
+      if (!user) {
+        errors.email = 'User not found';
+        return res.status(400).json(errors);
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            //User matched
+            const payload = { 
+              id: user.id, 
+              firstName: user.firstName, 
+              lastName: user.lastName,
+              avatar: user.avatar
+            }; // create JWT payload
+
+            //sign token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },// expires in 1 hr
+              (_, token) => {
+                res.json({
+                  success: true,
+                  toke: 'Bearer ' + token
+                });
+              }
+            );
+          } else {
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
+          }
+        });
+    });
+});
+
+// @route   GET api/users/current
+// @desc    Protected user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    email: req.user.email
+  });
+});
 
 module.exports = router;
