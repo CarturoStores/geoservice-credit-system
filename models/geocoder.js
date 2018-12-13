f2c595d785611adfe40c1435b8c349ed9d593bbd
David Perez <kitsunebrave@gmail.com>
2018-12-12 22:17:56 -0500

    api: create & fields

--- models/geocoder.js
+++ models/geocoder.js
@@ -2,6 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Geocoder = sequelize.define('Geocoder', {
     address: DataTypes.STRING,
+    zipcode: DataTypes.INTEGER,
+    latitude: DataTypes.STRING,
+    longitude: DataTypes.STRING,
+    state: DataTypes.STRING,
+    street_number: DataTypes.INTEGER,
     token: DataTypes.STRING
   }, {});
   Geocoder.associate = function(models) {
--- routes/api/address.js
+++ routes/api/address.js
@@ -3,26 +3,55 @@ require('dotenv').config({
   });
   const express = require('express');
   const router = express.Router();
-  const axios = require('axios');
-  const http = require('http');
+  const db = require('../../models');
 
   // Another alternative querying gooapis
   const requestify = require('requestify');
   
+  // @route   GET api/address/test
+  // @desc    Testing Address routes
+  // @access  Public
   router.get('/test', (req, res) => {
     res.json({
       msg: 'Router works'
     })
   });
 
-  router.get('/', (request, response) => {
-    const urlBase = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+parkway,+Mountain+View,+CA&key=${process.env.API_KEY}`;
+  // @route   GET api/address/geocode
+  // @desc    Receiving Geocoding data
+  // @access  Public
+  router.get('/geocode', (request, response) => {
+    const address = request.body.address;
+    const api_key = request.body.api_key;
+    urlBase = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${api_key || process.env.API_KEY}`;
     requestify.get(urlBase)
       .then(res => {
-        console.log(res.getBody());
         const result = res.getBody();
-        response.json({ currentGeoLocation: res.getBody() });
-    })
+        response.json({ currentGeoLocation: result });
+      })
+      .catch(err => {
+        return res.status(404).json({ err: 'Unexpected Error Ocurred' });
+      });
+  });
+
+  // @route   POST api/address/geocode
+  // @desc    Storing a new geocode record
+  // @access  Public
+  router.post('/geocode', (req, res) => {
+    db.Geocoder.create(Object.assign(req.body,{
+      address: req.body.address, // receive complete address
+      token: 'token', // generate Token
+      zip_code: req.body.zip_code, // zipCode
+      latitude: req.body.lat, // latitude
+      longitude: req.body.lng, // longitude
+      state: req.body.state, // state
+      street_number: req.body.street_number, // street number
+      createdAt: Date.now() // created date
+    }))
+    .then(geocoder => res.json(geocoder))
+    .catch(err => {
+      return res.status(400).send(err);
+    });
   });
   
   module.exports = router;
