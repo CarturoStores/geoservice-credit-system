ca12461f26735084ee5b71fea4eeb966741b796c
David Perez <kitsunebrave@gmail.com>
2018-12-12 20:46:12 -0500

    get geocoder data

--- routes/api/address.js
+++ routes/api/address.js
@@ -4,17 +4,25 @@ require('dotenv').config({
   const express = require('express');
   const router = express.Router();
   const axios = require('axios');
+  const http = require('http');
+
+  // Another alternative querying gooapis
+  const requestify = require('requestify');
   
   router.get('/test', (req, res) => {
     res.json({
       msg: 'Router works'
     })
   });
-  
-  router.get('/', (req, res) => {
-    const urlBase = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.API_KEY}`;
-    const url = axios(urlBase);
-    res.json({ currentLocation: 'Receiving address' });
+
+  router.get('/', (request, response) => {
+    const urlBase = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+parkway,+Mountain+View,+CA&key=${process.env.API_KEY}`;
+    requestify.get(urlBase)
+      .then(res => {
+        console.log(res.getBody());
+        const result = res.getBody();
+        response.json({ currentGeoLocation: res.getBody() });
+    })
   });
   
   module.exports = router;
