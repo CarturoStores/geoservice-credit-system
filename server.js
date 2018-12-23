e88adb2e188e70dec24e6780deced454e1ba1fce
David Perez <kitsunebrave@gmail.com>
2018-12-23 12:19:34 -0500

    deploy for heroku production org

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const passport = require('passport');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 app.use(morgan("tiny")); // logging framework
 
@@ -34,9 +35,14 @@ app.use('/api/address', address);
 app.use('/api/profile', profile);
 
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets (usually on heroku) if in production
 if (process.env.NODE_ENV === "production") {
+  // set static folder
   app.use(express.static("client/build"));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
