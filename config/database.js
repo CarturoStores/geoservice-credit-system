4af1796da8b81aa0a818835389be8b08a93c9e6d
David Perez <davidperez@Davids-MacBook-Pro.local>
2019-01-27 11:35:22 -0500

    missing files

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'credit_db',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
     port: 3306,
     dialect: 'mysql',
     operatorsAliases: false
--- routes/api/address.js
+++ routes/api/address.js
@@ -1,82 +1,104 @@
-require('dotenv').config({
-    silent: true
-  });
-  const express = require('express');
-  const router = express.Router();
-  const passport = require("passport");
-  const db = require('../../models');
+require("dotenv").config({
+  silent: true
+});
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const db = require("../../models");
 
-  // Another alternative querying gooapis
-  const requestify = require('requestify');
-  
-  // Load Input Validation
-  const validateGeocoderInput = require('../../validation/validate');
+// Another alternative querying gooapis
+const requestify = require("requestify");
 
-  // Load Geocoding model
-  const Address = db.Address;
-  const NodeGeocoder = require('node-geocoder');
- 
-  const options = {
-    provider: 'google',
-    // Optional depending on the providers
-    httpAdapter: 'https',         // Default
-    apiKey: process.env.API_KEY,  // for Mapquest, OpenCage, Google Premier
-    formatter: null               // 'gpx', 'string', ...
-  };
- 
-  const google = NodeGeocoder(options);
+// Load Input Validation
+const validateGeocoderInput = require("../../validation/validate");
 
-  // batch multiple geocoding addresses services excersices
-  
-  //Usage
-  // @route   GET api/address/all
-  // @desc    // Finding multiple address
-  // @access  Private
-  router.get('/all', passport.authenticate("jwt", { session: false }), async (req, res) => {
+// Load Geocoding model
+const Address = db.Address;
+const NodeGeocoder = require("node-geocoder");
+
+const options = {
+  provider: "google",
+  // Optional depending on the providers
+  httpAdapter: "https", // Default
+  apiKey: process.env.API_KEY, // for Mapquest, OpenCage, Google Premier
+  formatter: null // 'gpx', 'string', ...
+};
+
+const google = NodeGeocoder(options);
+
+// batch multiple geocoding addresses services excersices
+
+//Usage
+// @route   GET api/address/all
+// @desc    // Finding multiple address
+// @access  Private
+router.get(
+  "/all",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     // Do something after getting done with Geocoding of multiple addresses
-    let addresses = req.body.addresses.split('\n');
-    google.batchGeocode(addresses, (err, results) => {
-      // Return an array of type {error: false, value: []}
-      if (results) {      
-        res.json({
-          addresses: results
-        })
-      } else {
-        res.status(404).json({ error: 'Batch Geocoder failed'})
-      }
-    });
-  });
-  
-  // @route   GET api/address/test
-  // @desc    Testing Address routes
-  // @access  Public
-  router.get('/test', (req, res) => {
-    res.json({
-      msg: 'Address Router works'
-    })
-  });
+    let addresses = [];
+    if (req.body.addresses.split("\n").length !== 0) {
+      addresses = req.body.addresses.split("\n");
+    }
+    if (addresses) {
+      google.batchGeocode(addresses, (err, results) => {
+        // Return an array of type {error: false, value: []}
+        if (results) {
+          res.json({
+            addresses: results
+          });
+        } else {
+          res.status(404).json({ error: "Batch Geocoder failed" });
+        }
+      });
+    } else {
+      res.status(404).json({
+        response: "empty address array"
+      });
+    }
+  }
+);
 
-  // @route   GET api/address/geocode/
-  // @desc    Receiving Geocoding data
-  // @access  Private
-  router.get('/geocode/', passport.authenticate("jwt", { session: false }), async (request, response) => {
+// @route   GET api/address/test
+// @desc    Testing Address routes
+// @access  Public
+router.get("/test", (req, res) => {
+  res.json({
+    msg: "Address Router works"
+  });
+});
+
+// @route   GET api/address/geocode/
+// @desc    Receiving Geocoding data
+// @access  Private
+router.get(
+  "/geocode/",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
     const address = request.body.address;
     const api_key = request.body.api_key;
-    urlBase = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${api_key || process.env.API_KEY}`;
-    requestify.get(urlBase)
+    urlBase = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${api_key ||
+      process.env.API_KEY}`;
+    requestify
+      .get(urlBase)
       .then(res => {
         const result = res.getBody();
         response.json({ currentGeoLocation: result });
       })
       .catch(err => {
-        return res.status(404).json({ err: 'Unexpected Error Ocurred' });
+        return res.status(404).json({ err: "Unexpected Error Ocurred" });
       });
-  });
+  }
+);
 
-  // @route   GET api/address/timezone/
-  // @desc    Receiving Google Time Zone
-  // @access  Private
-  router.get('/timezone/', passport.authenticate("jwt", { session: false }), async (request, response) => {
+// @route   GET api/address/timezone/
+// @desc    Receiving Google Time Zone
+// @access  Private
+router.get(
+  "/timezone/",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
     const location = {
       lat: request.body.lat,
       lng: request.body.lng
@@ -84,22 +106,29 @@ require('dotenv').config({
     const timestamp = request.body.timestamp;
     const api_key = request.body.api_key;
     // const location = '39.6034810,-119.6822510';
-    urlBase = `https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=${api_key || process.env.API_KEY}`;
-    requestify.get(urlBase)
+    urlBase = `https://maps.googleapis.com/maps/api/timezone/json?location=${
+      location.lat
+    },${location.lng}&timestamp=${timestamp}&key=${api_key ||
+      process.env.API_KEY}`;
+    requestify
+      .get(urlBase)
       .then(res => {
         const result = res.getBody();
         response.json({ currentGooTimeZone: result });
       })
       .catch(err => {
-        return res.status(404).json({ err: 'Unexpected Error Ocurred' });
+        return res.status(404).json({ err: "Unexpected Error Ocurred" });
       });
-  });
+  }
+);
 
-  // @route   POST api/address/geocode
-  // @desc    Storing a new geocode record
-  // @access  Private
-  router.post('/create', passport.authenticate("jwt", { session: false }), async (req, res) => {
-
+// @route   POST api/address/geocode
+// @desc    Storing a new geocode record
+// @access  Private
+router.post(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     const find = {
       zip_code: req.body.zip_code,
       state: req.body.state
@@ -112,18 +141,22 @@ require('dotenv').config({
       return res.status(400).json(errors);
     }
 
-    Address.findOrCreate({ where: find, defaults: Object.assign(req.body,{
-      address: req.body.address, // receive complete address
-      token: 'token', // generate Token
-      zip_code: req.body.zip_code, // zipCode
-      latitude: req.body.latitude, // latitude
-      longitude: req.body.longitude, // longitude
-      state: req.body.state, // state
-      street_number: req.body.street_number, // street number
-      createdAt: Date.now() // created date
-    })})
-    .then(address => res.json(address))
-    .catch(err => console.log(err));
-  });
-  
-  module.exports = router;
+    Address.findOrCreate({
+      where: find,
+      defaults: Object.assign(req.body, {
+        address: req.body.address, // receive complete address
+        token: "token", // generate Token
+        zip_code: req.body.zip_code, // zipCode
+        latitude: req.body.latitude, // latitude
+        longitude: req.body.longitude, // longitude
+        state: req.body.state, // state
+        street_number: req.body.street_number, // street number
+        createdAt: Date.now() // created date
+      })
+    })
+      .then(address => res.json(address))
+      .catch(err => console.log(err));
+  }
+);
+
+module.exports = router;
